35288c9178f411d3d4b5d1db374759e74b1e1f91
Johnathon <johnathondouglass@gmail.com>
2023-12-21 16:50:24 -0800

    implement system status endpoint

---
+++ web/collection-coffee/src/app/api/v1/system-status/route.ts
@@ -0,0 +1,29 @@
+import { pool } from "@/app/lib/db/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const client = await pool.connect();
+
+    const systemStatusQuery = `
+      SELECT v.name, r.start_time, r.end_time
+      FROM runtime r
+      JOIN vendor v ON r.vendor_id = v.id
+      WHERE r.end_time IS NOT NULL;
+    `;
+
+    const systemStatus = (await client.query(systemStatusQuery)).rows;
+
+    client.release();
+
+    return NextResponse.json({
+      systemStatus,
+    });
+  } catch (error) {
+    console.error("Database query failed:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch system status data" },
+      { status: 500 }
+    );
+  }
+}
