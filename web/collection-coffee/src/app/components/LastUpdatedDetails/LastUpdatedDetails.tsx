89c5353f05d5fa51a8f6fd992e6944f748b3c673
Johnathon <johnathondouglass@gmail.com>
2024-01-14 16:21:57 -0800

    fix last updated formatting

--- web/collection-coffee/src/app/components/LastUpdatedDetails/LastUpdatedDetails.tsx
+++ web/collection-coffee/src/app/components/LastUpdatedDetails/LastUpdatedDetails.tsx
@@ -10,7 +10,16 @@ const LastUpdatedDetails = ({
   isScraperRunning,
 }: LastUpdatedDetailsProps) => {
   const localDate = new Date(lastUpdatedDateTime);
-  const formattedDate = localDate.toLocaleString();
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  };
+  const formattedDate = localDate.toLocaleString("en-US", options);
   return (
     <div className="last-updated-details-container">
       <div className="last-updated-details">
